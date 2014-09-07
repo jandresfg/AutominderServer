a1a9e8fb2b050683562160dfd04e741ed5821457
Andres Fernandez <jandresfg@hotmail.com>
2014-09-07 10:07:47 -0500

    Prueba commit (commit 2)

--- com.jandresfg.git.first/src/com/jandresfg/git/first/GitTest.java
+++ com.jandresfg.git.first/src/com/jandresfg/git/first/GitTest.java
@@ -3,7 +3,7 @@ package com.jandresfg.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Git is fun");
+		System.out.println("Git is cool");
 	}
 
 }
